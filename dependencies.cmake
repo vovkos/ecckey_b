49f4381809b008a94f076571f51558a47d66ea71
Vladimir Gladkov <vovkos@gmail.com>
2016-10-28 15:09:29 +0000

    initial commit

---
+++ dependencies.cmake
@@ -0,0 +1,28 @@
+#...............................................................................
+#
+#  This file is part of the ECCKey utility.
+#
+#  ECCKey is distributed under the MIT license.
+#  For details see accompanying license.txt file,
+#  the public copy of which is also available at:
+#  http://tibbo.com/downloads/archive/ecckey/license.txt
+#
+#...............................................................................
+
+set (
+	AXL_PATH_LIST
+
+	OPENSSL_INC_DIR
+	OPENSSL_LIB_DIR
+	RAGEL_EXE
+	)
+
+set (
+	AXL_IMPORT_LIST
+
+	REQUIRED
+		openssl
+		ragel
+	)
+
+#...............................................................................
